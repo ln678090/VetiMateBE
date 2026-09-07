9fe79f0974d26524fbf448b529529f49a5c5978e
ln678090 <ln678090@gmail.com>
2026-09-07 11:02:57 +0700

    fix formatt

--- src/test/java/com/graduation/project/SchemaValidationTest.java
+++ src/test/java/com/graduation/project/SchemaValidationTest.java
@@ -2,27 +2,15 @@ package com.graduation.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("ci")
 class SchemaValidationTest {
+  @MockitoBean private RedisTemplate<String, String> redisTemplate;
 
-  // ❌ XÓA hoặc COMMENT dòng Mock RsaKeyProperties cũ đi
-  // @MockBean private RsaKeyProperties rsaKeyProperties;
-
-  // 🟢 BỔ SUNG 1: Mock trực tiếp Bean JWKSource để Spring Boot không chạy vào
-  // logic của JWKConfig nữa
-  // @MockBean private JWKSource<SecurityContext> jwkSource;
-  //
-  // // 🟢 BỔ SUNG 2: Mock luôn bộ mã hóa JwtEncoder để TokenService không bị lỗi
-  // // tiêm phụ thuộc
-  // @MockBean private JwtEncoder jwtEncoder;
-  //
-  // // Giữ nguyên các Mock Bean Redis cũ
-  // @MockBean private StringRedisTemplate stringRedisTemplate;
-  // @MockBean private RedisTemplate<String, String> redisTemplate;
-  //
   @Test
   void contextLoads() {
     // Nếu Spring context khởi động thành công nghĩa là:
